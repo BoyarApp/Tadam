320b9ff73410fd7d2537b711494771ef3c8ec5b8
Yukta <>
2025-10-24 01:05:22 +0530

    test: mock useRuntimeConfig in vitest setup
    Add global mock for useRuntimeConfig to provide test environment with apiBase configuration

--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -1,5 +1,12 @@
 import { config } from '@vue/test-utils';
+import { vi } from 'vitest';
 
 config.global.mocks = {
   $t: (msg: string) => msg,
 };
+
+vi.stubGlobal('useRuntimeConfig', () => ({
+  public: {
+    apiBase: 'http://localhost:1337',
+  },
+}));
